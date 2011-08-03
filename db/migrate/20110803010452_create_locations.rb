af5c5b4866c40b28dc22ea189d21e539158bc626
Sam Grossberg <sam.grossberg@gmail.com>
2011-08-02 18:11:51 -0700

    Scaffolds

---
+++ db/migrate/20110803010452_create_locations.rb
@@ -0,0 +1,11 @@
+class CreateLocations < ActiveRecord::Migration
+  def change
+    create_table :locations do |t|
+      t.string :title
+      t.text :description
+      t.integer :category_id
+
+      t.timestamps
+    end
+  end
+end
