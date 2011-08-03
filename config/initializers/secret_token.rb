dcbc70ceec7d370be753e72f508ddc0e9b32a611
Sam Grossberg <sam.grossberg@gmail.com>
2011-08-02 18:03:52 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seattle::Application.config.secret_token = '47e0c926933e5d40a6e332e9ad6077732551706279dc98cba301d0eeb631c7b7743b44f63822192f66451a1d1f853dc2aadf08ac7059c926ff29e3aa2699e7bd'
